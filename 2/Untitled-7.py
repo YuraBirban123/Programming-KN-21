6e3ca070b5752753bf1a62ba336423013292617b
YuraBirban123 <urabirban123@gmail.com>
2024-11-05 14:30:45 +0200

    add new

---
+++ 2/Untitled-7.py
@@ -0,0 +1,9 @@
+while True:
+    msg=input("Please input massage:")
+    if msg=="Добрий день":
+        print("Добрий день")
+    elif msg=="Слава Україні":
+        print("Героям Слава")
+    elif msg=="Па-па":
+        print("The end")
+        break
